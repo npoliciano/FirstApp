8aa81d6854a45d547862e211911546bac4018d31
Nicolle Policiano <nicolle.fabri@gmail.com>
2023-08-28 21:31:16 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Nicolle on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
